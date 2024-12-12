659083a7202aade3bcdd54cac247ee681b792a03
Shafaque Sattar <shafaque.sattar@quartett-mobile.de>
2024-12-12 18:29:07 +0100

    Add dependency, image and text

--- automotive/src/main/java/de/quartett/mobile/roadgallery/MainActivity.kt
+++ automotive/src/main/java/de/quartett/mobile/roadgallery/MainActivity.kt
@@ -1,13 +1,69 @@
 package de.quartett.mobile.roadgallery
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposeexample.ui.theme.RoadGalleryTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            RoadGalleryTheme {
+                // A surface container using the 'background' color from the theme
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    // Loading an image resource from drawable
+    val image: Painter = painterResource(id = R.drawable.ic_launcher_foreground)
+
+    // Column to arrange the Text and Image vertically
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(16.dp).fillMaxSize()
+    ) {
+        // Displaying the Text
+        Text(
+            text = "Welcome to QM!",
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        // Displaying the Image
+        Image(
+            painter = image,
+            contentDescription = "Sample Image",
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RoadGalleryTheme {
+        MainScreen()
     }
 }
